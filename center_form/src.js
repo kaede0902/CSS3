8902ebafdf52d08fb4a177835107d3315481808b
kaede0902 <kaede0920js@gmail.com>
2019-09-20 15:53:24 +0900

    add centered form

--- center_form/src.js
+++ center_form/src.js
@@ -1,4 +1,18 @@
-let changeTest = () =>
-    let char = document.getElementById('area1').value;
-    alert(`Changed`);
-// this executed when cursor moved from there.
+let reg = {
+    ykhm: '/045-d{3}-d{4}',
+    tko: '/030-d{4}-d{4}',
+}
+
+let form = document.getElementById('form');
+form.onsubmit = () => {
+    let val = form.hoge.value;
+    console.log(val);
+    return false;
+}
+let judge = (val) => {
+    let region;
+    if (val.match(reg.ykhm))
+        region = 'Yokohama';
+    else if (val.match(reg.tko))
+        region = 'Tokyo';
+}
